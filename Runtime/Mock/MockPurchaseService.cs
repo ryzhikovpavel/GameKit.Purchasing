80ea6fb0d8e5965f9dad7a00551df60fa039bcab
ryzhikovpavel <ryzhikovpavel@gmail.com>
2022-11-01 18:18:15 +0400

    change mock service

--- Runtime/Mock/MockPurchaseService.cs
+++ Runtime/Mock/MockPurchaseService.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace GameKit.Purchasing.Mock
 {
     public class MockPurchaseService<TProduct>: IPurchaseService<TProduct> where TProduct : class, IProductItem
     {
+        private TProduct[] _products;
         public event Action<ITransaction<TProduct>> EventTransactionBegin;
 
         public event Action<ITransaction<TProduct>> EventTransactionSuccess;
@@ -21,41 +23,59 @@ namespace GameKit.Purchasing.Mock
 
         public event Action<TProduct> EventProductPurchased;
 
-        public bool IsInitialized { get; }
+        public bool IsInitialized { get; private set; }
+
+        public async Task Initialize(params TProduct[] products)
+        {
+            _products = products;
+            await Task.Delay(1000);
+            IsInitialized = true;
+            Debug.Log($"IAP initialized");
+        }
 
         public bool FindProduct(string productId, out TProduct product)
         {
-            throw new NotImplementedException();
-        }
+            foreach (TProduct item in _products)
+            {
+                if (item.Id == productId)
+                {
+                    product = item;
+                    return true;
+                }
+            }
 
-        public Task Initialize(params TProduct[] products)
-        {
-            throw new NotImplementedException();
+            product = default;
+            return false;
         }
 
         public void Confirm(TProduct product)
         {
-            throw new NotImplementedException();
+            Debug.Log($"IAP Purchase: {product.Id}");
+            if (product.Type == ProductItemType.Consumable)
+                product.Status = ProductStatus.Ready;
+            else
+                product.Status = ProductStatus.Purchased;
         }
 
         public void Restore()
         {
-            throw new NotImplementedException();
+            Debug.Log("IAP Restore");
         }
 
-        public Task<ITransaction<TProduct>> Purchase(Transaction<TProduct> transaction)
+        public async Task<ITransaction<TProduct>> Purchase(Transaction<TProduct> transaction)
         {
-            throw new NotImplementedException();
+            Debug.Log($"IAP Purchase: {transaction.Product.Id}");
+            transaction.State = TransactionState.Processing;
+            await Task.Delay(1000);
+            transaction.State = TransactionState.Pending;
+            transaction.Product.Status = ProductStatus.Pending;
+            return transaction;
         }
 
         public IEnumerator<TProduct> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => ((IEnumerable<TProduct>)_products).GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+            => GetEnumerator();
     }
 }
